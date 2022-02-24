3810b53e7e78f147de2b42988ed6eeb499e9a802
Travis <tzito93@gmail.com>
2022-02-24 11:24:46 -0600

    Computer player random code generation using the Board classe's set of colored 'pegs'

--- board.rb
+++ board.rb
@@ -1,9 +1,14 @@
 class Board
+  @@colors = [:R, :G, :B, :Y] #red, green, blue, yellow
+  
+  def self.colors
+    @@colors
+  end
+  
   def initialize(length)
     @length = length
     @rows = Array.new(@length) { Array.new(4, '_') }
-    @colors = [:R, :G, :B, :Y] #red, green, blue, yellow
-  end
+  end 
 
   def render
     @rows.each do |row|
---
+++ computer_player.rb
@@ -0,0 +1,15 @@
+require_relative 'board'
+
+class ComputerPlayer
+  attr_reader :code
+  
+  def initialize
+    @code = generate_code
+  end
+
+  def generate_code
+    code = []
+    4.times { code << Board.colors.sample }
+    code
+  end
+end
