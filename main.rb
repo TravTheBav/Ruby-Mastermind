8061d6af99142550d3c50aa6f46e7c0f43daf114
Travis <tzito93@gmail.com>
2022-03-03 11:28:06 -0600

    reverted replit changes

--- main.rb
+++ main.rb
@@ -1,12 +1,6 @@
 # running this file in the terminal will start a game of mastermind
 
 require_relative 'game'
-require 'bundler/inline'
-
-gemfile true do
- source 'http://rubygems.org'
- gem 'colorize'
-end
 
 game = Game.new
 game.play
