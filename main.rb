8810cace75c57a6c787606656a55756174a951c7
Travis <tzito93@gmail.com>
2022-03-03 11:17:21 -0600

    gem installed through bundle/inline for replit compatability

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 # running this file in the terminal will start a game of mastermind
 
 require_relative 'game'
+require 'bundler/inline'
+
+gemfile true do
+ source 'http://rubygems.org'
+ gem 'colorize'
+end
 
 game = Game.new
 game.play
